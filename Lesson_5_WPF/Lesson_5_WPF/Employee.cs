b231a2326bb3c057d67eb071be7a48650d5300ab
ap-konovalov <sanches-zi@mail.ru>
2019-05-20 21:11:11 +0300

    Добавлена сущность сотрудник

---
+++ Lesson_5_WPF/Lesson_5_WPF/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5_WPF
+{
+    public class Employee
+    {
+        public int _id { get; set;}
+        private string _firstName { get; set; }
+        private string _lastName { get; set; }
+        private string _patronymic { get; set; }
+        private int _age { get; set;}
+
+        public Employee(int Id,string FirstName, string LastName, string Patronymic, int Age)
+        {
+            _id = Id;
+            _firstName = FirstName;
+            _lastName = LastName;
+            _patronymic = Patronymic;
+            _age = Age;
+        }
+
+        public override string ToString()
+        {
+            return $"{_id}. {_firstName} {_lastName} {_patronymic} {_age}";
+        }
+
+
+        public ObservableCollection<Employee> employeesList = new ObservableCollection<Employee>();
+        public void AddEmplyee()
+        {
+            employeesList.Add(new Employee(_id, _firstName, _lastName, _patronymic, _age));
+        }
+    }
+}
---
+++ Lesson_5_WPF/Lesson_5_WPF/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lesson_5_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Employee Employee_1 = new Employee(1, "Ivan", "Kyzmin", "Ivanovich", 18);
+            Employee Employee_2 = new Employee(2, "Petr", "Zaicev", "Igorevich", 35);
+            Employee Employee_3 = new Employee(3, "Konstantin", "Averkin", "Petrovich", 49);
+        }
+    }
+}
